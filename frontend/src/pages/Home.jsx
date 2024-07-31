a03f20ccb17b3241592b39132f0187020a4550c3
0801IT211001 <aayush.anand6263@gmail.com>
2024-07-31 16:17:48 +0530

    aayush 1

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+      <main>
+        <h1>Welcome to Our Custom Art System</h1>
+        <p>Explore our gallery, learn about our system, or create your own art!</p>
+      </main>
+    );
+  }
+  
+  export default Home;
